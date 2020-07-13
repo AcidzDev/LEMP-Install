0c95b804fd2f05c01d072526102a72e9861628bc
Jamie McInnes <41684417+AcidzDev@users.noreply.github.com>
2020-07-13 01:48:44 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -22,8 +22,8 @@ service nginx restart
 # Install pHp 7.2
 add-apt-repository -y ppa:ondrej/php -y
 apt update
-apt -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip
-sed -i 's/;cgi\.fix_pathinfo=1/cgi\.fix_pathinfo=0/g' /etc/php/7.2/fpm/php.ini
+apt -y install php7.4 php7.4-cli php7.4-gd php7.4-mysql php7.4-pdo php7.4-mbstring php7.4-tokenizer php7.4-bcmath php7.4-xml php7.4-fpm php7.4-curl php7.4-zip
+sed -i 's/;cgi\.fix_pathinfo=1/cgi\.fix_pathinfo=0/g' /etc/php/7.4/fpm/php.ini
 
 # Install MariaDB
 apt install mariadb-server mariadb-client
